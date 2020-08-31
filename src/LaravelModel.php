1f8d301105fc63cec41f72db8016f091c62ff26d
damon <644577519@qq.com>
2020-08-31 17:32:52 +0800

    3

--- src/LaravelModel.php
+++ src/LaravelModel.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 
 class LaravelModel extends Model
 {
+
     use SoftDeletes;
 
 
@@ -22,42 +23,43 @@ class LaravelModel extends Model
      *
      * @author wumengmeng <wu_mengmeng@foxmail.com>
      */
-    private function demo(){
+    private function demo()
+    {
         $option = [
-          'with'=>['aa','bb'],
-          'where'=>[
-            'is_menu'=>1,//类型1
-            ['status','!=',2],//类型2
-            ['name','like','习俗'],//类型3
+          'with'       => ['aa', 'bb'],
+          'where'      => [
+            'is_menu' => 1,//类型1
+            ['status', '!=', 2],//类型2
+            ['name', 'like', '习俗'],//类型3
           ],
-          'whereIn'=>[
-            'role_id'=>[6,8,22],
-            'user_name'=>['张三','李四']
+          'whereIn'    => [
+            'role_id'   => [6, 8, 22],
+            'user_name' => ['张三', '李四'],
           ],
-          'whereNotIn'=>[
-            'role_id'=>[6,8,22],
-            'user_name'=>['张三','李四']
+          'whereNotIn' => [
+            'role_id'   => [6, 8, 22],
+            'user_name' => ['张三', '李四'],
           ],
-          'orWhere'=>[
-            'is_menu'=>1,//类型1
-            ['status','!=',2],//类型2
-            ['name','like','习俗'],//类型3
+          'orWhere'    => [
+            'is_menu' => 1,//类型1
+            ['status', '!=', 2],//类型2
+            ['name', 'like', '习俗'],//类型3
           ],
-          'field'=>['id','name','title'],
-          'withCount'=>['aa','bb'],
+          'field'      => ['id', 'name', 'title'],
+          'withCount'  => ['aa', 'bb'],
           //order排序，谁在前谁优先
-          'orderBy'=>[
-            'sort'=>'asc',
-            'id'=>'asc'
+          'orderBy'    => [
+            'sort' => 'asc',
+            'id'   => 'asc',
           ],
-          'limit'=>15,
-          'sum'=>'score',
-          'whereHas'    => [
+          'limit'      => 15,
+          'sum'        => 'score',
+          'whereHas'   => [
             'user.goods' => [
               'where'   => [
-                'is_menu'=>1,//类型1
-                ['status','!=',2],//类型2
-                ['name','like','习俗'],//类型3
+                'is_menu' => 1,//类型1
+                ['status', '!=', 2],//类型2
+                ['name', 'like', '习俗'],//类型3
               ],
               'whereIn' => [
                 'role_id'   => [6, 8, 22],
@@ -76,13 +78,14 @@ class LaravelModel extends Model
      * @return array
      * @author wumengmeng <wu_mengmeng@foxmail.com>
      */
-    protected function laravel_table_fields()
+    public function laravel_table_fields()
     {
-        $prefix = DB::getConfig('prefix');
-        $table = $this->laravel_child_model()->table;
+        $prefix       = DB::getConfig('prefix');
+        $table        = $this->laravel_child_model()->table;
         $s_full_table = $prefix . $table;
-        $columns = DB::getDoctrineSchemaManager()->listTableColumns($s_full_table);
-        $arr_data = [];
+        $columns      = DB::getDoctrineSchemaManager()
+                          ->listTableColumns($s_full_table);
+        $arr_data     = [];
         foreach ($columns as $column) {
             $arr_data[] = $column->getName();
         }
@@ -90,32 +93,36 @@ class LaravelModel extends Model
     }
 
     /* 基础 option */
-    private function laravel_option($option = []) {
-        $option = array_change_key_case($option,CASE_LOWER);
-        $option['field'] = isset($option['field']) ? $option['field'] : ['*'];//默认查询字段
+    private function laravel_option($option = [])
+    {
+        $option            = array_change_key_case($option, CASE_LOWER);
+        $option['field']   = isset($option['field']) ? $option['field'] : ['*'];//默认查询字段
         $option['orderby'] = isset($option['orderby']) ? $option['orderby'] : [];//多个排序方式
-        $option['limit'] = isset($option['limit']) ? $option['limit'] : 10;//默认每页查询条数
+        $option['limit']   = isset($option['limit']) ? $option['limit'] : 10;//默认每页查询条数
         return $option;
     }
 
-    private function where_option(){
+    private function where_option()
+    {
         return yoo_array_value_lower(['where', 'orWhere', 'whereIn', 'whereNotIn', 'whereHas']);
     }
 
     /* with */
-    private function laravel_with($query, $arr_data) {
+    private function laravel_with($query, $arr_data)
+    {
         $query = $query->with($arr_data);
         return $query;
     }
 
     /* where */
-    private function laravel_where($query, $arr_data) {
+    private function laravel_where($query, $arr_data)
+    {
         foreach ($arr_data as $key => $value) {
             if (is_array($value)) {
-                if($value[1] == 'like'){
+                if ($value[1] == 'like') {
                     $query = $query->where($key, 'like', '%' . $value . '%');
                 }
-                else{
+                else {
                     $query = $query->where($value[0], $value[1], $value[2]);
                 }
             }
@@ -127,13 +134,14 @@ class LaravelModel extends Model
     }
 
     /* orWhere */
-    private function laravel_orwhere($query, $arr_data) {
+    private function laravel_orwhere($query, $arr_data)
+    {
         foreach ($arr_data as $key => $value) {
             if (is_array($value)) {
-                if($value[1] == 'like'){
+                if ($value[1] == 'like') {
                     $query = $query->orWhere($key, 'like', '%' . $value . '%');
                 }
-                else{
+                else {
                     $query = $query->orWhere($value[0], $value[1], $value[2]);
                 }
             }
@@ -145,9 +153,10 @@ class LaravelModel extends Model
     }
 
     /* whereIn */
-    private function laravel_wherein($query, $arr_data) {
+    private function laravel_wherein($query, $arr_data)
+    {
         foreach ($arr_data as $key => $value) {
-            if(!empty($value)){
+            if (!empty($value)) {
                 $query = $query->whereIn($key, $value);
             }
         }
@@ -155,9 +164,10 @@ class LaravelModel extends Model
     }
 
     /* whereNotIn */
-    private function laravel_wherenotin($query, $arr_data) {
+    private function laravel_wherenotin($query, $arr_data)
+    {
         foreach ($arr_data as $key => $value) {
-            if(!empty($value)) {
+            if (!empty($value)) {
                 $query = $query->whereNotIn($key, $value);
             }
         }
@@ -165,19 +175,22 @@ class LaravelModel extends Model
     }
 
     /* select */
-    private function laravel_select($query, $arr_data) {
+    private function laravel_select($query, $arr_data)
+    {
         $query = $query->select($arr_data);
         return $query;
     }
 
     /* withCount */
-    private function laravel_withcount($query, $arr_data) {
+    private function laravel_withcount($query, $arr_data)
+    {
         $query = $query->withCount($arr_data);
         return $query;
     }
 
     /* orderBy */
-    private function laravel_order($query, $arr_data) {
+    private function laravel_order($query, $arr_data)
+    {
         foreach ($arr_data as $key => $value) {
             $query = $query->orderBy($key, $value);
         }
@@ -185,7 +198,8 @@ class LaravelModel extends Model
     }
 
     /* whereHas */
-    private function laravel_wherehas($query, $arr_data) {
+    private function laravel_wherehas($query, $arr_data)
+    {
         foreach ($arr_data as $key => $value) {
             $query = $query->whereHas($key,
               function ($qqqqq)
@@ -195,17 +209,17 @@ class LaravelModel extends Model
                   foreach ($value as $kk => $vv) {
                       switch ($kk) {
                           case 'where':
-                              $qqqqq = $this->laravel_where($qqqqq,$vv);
+                              $qqqqq = $this->laravel_where($qqqqq, $vv);
                               break;
                           case 'orwhere':
-                              $qqqqq = $this->laravel_orwhere($qqqqq,$vv);
+                              $qqqqq = $this->laravel_orwhere($qqqqq, $vv);
                               break;
                           case 'wherein':
-                              $qqqqq = $this->laravel_wherein($qqqqq,$vv);
+                              $qqqqq = $this->laravel_wherein($qqqqq, $vv);
                               break;
                           case 'wherenotin':
                               // 注：当关联关系是1对多时，whereNotIn效果有限，只有关联里有一个数据不符合whereNotIn条件，该数据仍然会显示
-                              $qqqqq = $this->laravel_wherenotin($qqqqq,$vv);
+                              $qqqqq = $this->laravel_wherenotin($qqqqq, $vv);
                               break;
 
                           default:
@@ -226,12 +240,12 @@ class LaravelModel extends Model
      * @return mixed
      * @author wumengmeng <wu_mengmeng@foxmail.com>
      */
-    private function laravel_query($query, &$option = [],$arr_remain = [])
+    private function laravel_query($query, &$option = [], $arr_remain = [])
     {
         $option = $this->laravel_option($option);
-        if(!empty($arr_remain)){
+        if (!empty($arr_remain)) {
             $arr_remain = yoo_array_value_lower($arr_remain);
-            $option = yoo_array_remain($option,$arr_remain);
+            $option     = yoo_array_remain($option, $arr_remain);
         }
 
         $query = !empty($option['with']) ? $this->laravel_with($query, $option['with']) : $query;
@@ -247,8 +261,16 @@ class LaravelModel extends Model
 
     }
 
-    public function scopeLaravelOption($query,$option = []){
-        $result = $this->laravel_query($query,$option);
+    public function scopeLaravelOption($query, $option = [])
+    {
+        $result = $this->laravel_query($query, $option);
+        return $result;
+    }
+
+    public function scopeLaravelWhereOption($query, $option = [])
+    {
+        $arr_remain = $this->where_option();
+        $result     = $this->laravel_query($query, $option, $arr_remain);
         return $result;
     }
 
@@ -261,18 +283,23 @@ class LaravelModel extends Model
      * @return mixed
      * @author wumengmeng <wu_mengmeng@foxmail.com>
      */
-    public function scopeLaravelAll($query, $option = []) {
-        return  $this->laravel_query($query,$option)->get();
+    public function scopeLaravelAll($query, $option = [])
+    {
+        return $this->laravel_query($query, $option)
+                    ->get();
     }
 
     /**
      * 获取列表数据-含分页
-     * @param $query
+     * @param       $query
      * @param array $option
+     *
      * @return mixed
      */
-    public function scopeLaravelList($query, $option = []) {
-        return $this->laravel_query($query,$option)->paginate($option['limit']);
+    public function scopeLaravelList($query, $option = [])
+    {
+        return $this->laravel_query($query, $option)
+                    ->paginate($option['limit']);
     }
 
     /**
@@ -285,8 +312,11 @@ class LaravelModel extends Model
      * @return mixed
      * @author wumengmeng <wu_mengmeng@foxmail.com>
      */
-    public function scopeLaravelFind($query, $id = 0, $option = []) {
-        return $this->laravel_query($query,$option,['with','field','withCount'])->find($id);
+    public function scopeLaravelFind($query, $id = 0, $option = [])
+    {
+        //查询不存在的数据时，不返回null 最好使用 laravelOption()->find()
+        return $this->laravel_query($query, $option, ['with', 'field', 'withCount'])
+                    ->find($id);
     }
 
 
@@ -300,45 +330,54 @@ class LaravelModel extends Model
      * @return mixed
      * @author wumengmeng <wu_mengmeng@foxmail.com>
      */
-    public function scopeLaravelUpdate($query,  $data = [],  $option = []) {
-        return $this->laravel_query($query,$option)->update($data);
+    public function scopeLaravelUpdate($query, $data = [], $option = [])
+    {
+        return $this->laravel_query($query, $option)
+                    ->update($data);
     }
 
     /**
      * 查询一条数据(通过where条件查询)
-     * @param $query
+     *
+     * @param       $query
      * @param array $option
+     *
      * @return array
      */
-    public function scopeLaravelOne($query, $option = []) {
-        return $this->laravel_query($query,$option)->first();
+    public function scopeLaravelOne($query, $option = [])
+    {
+        return $this->laravel_query($query, $option)
+                    ->first();
     }
 
     /**
      * 获取统计数量
      *
      * @param       $query
-     * @param array $arr_option
+     * @param array $option
      *
      * @return mixed
      * @author wumengmeng <wu_mengmeng@foxmail.com>
      */
-    public function scopeLaravelCount($query, $option = []) {
-        return $this->laravel_query($query,$option)->count();
+    public function scopeLaravelCount($query, $option = [])
+    {
+        return $this->laravel_query($query, $option)
+                    ->count();
     }
 
     /**
      * 获取统计总和
      *
      * @param       $query
-     * @param array $arr_option
+     * @param array $option
      *
      * @return mixed
      * @author wumengmeng <wu_mengmeng@foxmail.com>
      */
     public function scopeLaravelSum($query, $option = [])
     {
-        return $this->laravel_query($query,$option)->sum($option['sum']);
+        return $this->laravel_query($query, $option)
+                    ->sum($option['sum']);
 
     }
 
@@ -352,17 +391,17 @@ class LaravelModel extends Model
      * @return mixed
      * @author wumengmeng <wu_mengmeng@foxmail.com>
      */
-    public function scopeLaravelDelete($query,$option = [],  $bool = true)
+    public function scopeLaravelDelete($query, $option = [], $bool = true)
     {
         $arr_remain = $this->where_option();
-        $result = $this->laravel_query($query,$option,$arr_remain);
+        $result     = $this->laravel_query($query, $option, $arr_remain);
         if ($bool === true) {
             return $result->forceDelete();  //物理删除
-        } else {
+        }
+        else {
             return $result->delete();       //软删除
         }
     }
-
 
 
 }
--- src/LaravelRepository.php
+++ src/LaravelRepository.php
@@ -10,100 +10,119 @@ namespace HashyooFast;
 
 class LaravelRepository
 {
-    /**
-     * 模型名称
-     * @return mixed
-     */
-    public function laravel_child_model()
+
+    public function __construct()
     {
-        $child_model_class = get_called_class();
-        $model = new $child_model_class;
-        return $model->baseModel();
+        $this->laravel_model = $this->baseModel();
     }
+
+    //    /**
+    //     * 模型名称
+    //     * @return mixed
+    //     */
+    //    public function laravel_child_model()
+    //    {
+    //        $child_model_class = get_called_class();
+    //        $model = new $child_model_class;
+    //        return $model->baseModel();
+    //    }
 
     /**
      * 查询一条数据-根据主键查询
-     * @param int $id
+     * @param int   $id
      * @param array $arr_option
+     *
      * @return mixed
      */
-    public function find( $id, $arr_option = [])
+    public function find($id, $arr_option = [])
     {
-        return $this->laravel_child_model()->laravelFind($id, $arr_option);
+        return $this->laravel_model->laravelFind($id, $arr_option);
     }
 
     /**
      * 查询一条数据-根据where条件查询
      * @param array $arr_option
+     *
      * @return mixed
      */
     public function get_one($arr_option = [])
     {
-        return $this->laravel_child_model()->laravelOne($arr_option);
+        return $this->laravel_model->laravelOne($arr_option);
     }
 
 
     /**
      * 查询列表-含分页
      * @param array $arr_option
+     *
      * @return mixed
      */
     public function get_list($arr_option = [])
     {
-        return $this->laravel_child_model()->laravelList($arr_option);
+        return $this->laravel_model->laravelList($arr_option);
     }
 
     /**
      * 查询所有数据
+     *
      * @param array $arr_option
+     *
      * @return mixed
      */
     public function get_all($arr_option = [])
     {
-        return $this->laravel_child_model()->laravelAll($arr_option);
+        return $this->laravel_model->laravelAll($arr_option);
     }
 
     /**
      * 添加一条数据
+     *
      * @param $post_data
+     *
      * @return mixed
      */
     public function add_one($post_data = [])
     {
-        $model = $this->laravel_child_model();
+        $model     = $this->laravel_model;
         $post_data = yoo_array_remain_trim($post_data, $model->laravel_table_fields());
-        return $model->create($post_data);
+        return $model->create($post_data); //返回插入数据
     }
 
     /**
      * 添加多条数据
+     *
      * @param $post_data
+     *
      * @return mixed
      */
     public function add_many($post_data = [])
     {
-        return $this->laravel_child_model()->insert($post_data);
+        return $this->laravel_model->insert($post_data);//返回 true 或 false
     }
 
     /**
      * 根据主键id修改一条数据
+     *
      * @param array $update_data
+     *
      * @return mixed
      */
-    public function update($update_data = [],$arr_option = [])
+    public function update($update_data = [], $arr_option = [])
     {
-        return $this->laravel_child_model()->laravelUpdate($update_data,$arr_option);
+        return $this->laravel_model->laravelUpdate($update_data, $arr_option);//返回影响行数
     }
 
     /**
      * 通过属性找到相匹配的数据并更新，如果不存在即创建
+     *
      * @param array $where
      * @param array $update_data
+     *
      * @return mixed
      */
     public function update_or_create($where = [], $update_data = [])
     {
-        return $this->laravel_child_model()->updateOrCreate($where, $update_data);
+        return $this->laravel_model->updateOrCreate($where, $update_data); //返回插入或修改数据 只会影响或者插入一条数据
     }
 
 
@@ -117,7 +136,7 @@ class LaravelRepository
      */
     public function get_count($arr_option = [])
     {
-        return $this->laravel_child_model()->laravelCount($arr_option);
+        return $this->laravel_model->laravelCount($arr_option);
     }
 
     /**
@@ -130,7 +149,7 @@ class LaravelRepository
      */
     public function get_sum($arr_option = [])
     {
-        return $this->laravel_child_model()->laravelSum($arr_option);
+        return $this->laravel_model->laravelSum($arr_option);
     }
 
 
@@ -143,10 +162,25 @@ class LaravelRepository
      * @return mixed
      * @author wumengmeng <wu_mengmeng@foxmail.com>
      */
-    public function delete_one( $id = 0,  $bool = false)
+    public function delete_one($id = 0)
     {
-        $arr_option = ['where'=>['id'=>$id]];
-        return $this->laravel_child_model()->laravelDelete($arr_option, $bool);
+        $arr_option = ['where' => ['id' => $id]];
+        return $this->laravel_model->laravelDelete($arr_option, false);
+    }
+
+    /**
+     * 根据主键id删除一条数据
+     *
+     * @param int  $id
+     * @param bool $bool
+     *
+     * @return mixed
+     * @author wumengmeng <wu_mengmeng@foxmail.com>
+     */
+    public function del_one_true($id = 0)
+    {
+        $arr_option = ['where' => ['id' => $id]];
+        return $this->laravel_model->laravelDelete($arr_option, true);
     }
 
     /**
@@ -158,34 +192,88 @@ class LaravelRepository
      * @return mixed
      * @author wumengmeng <wu_mengmeng@foxmail.com>
      */
-    public function delete($arr_option = [],  $bool = false)
+    public function delete($arr_option = [])
     {
-        return $this->laravel_child_model()->laravelDelete($arr_option, $bool);
+        return $this->laravel_model->laravelDelete($arr_option, false);
+    }
+
+    /**
+     * 根据条件删除数据
+     *
+     * @param array $arr_option
+     *
+     * @return mixed
+     * @author wumengmeng <wu_mengmeng@foxmail.com>
+     */
+    public function del_true($arr_option = [])
+    {
+        return $this->laravel_model->laravelDelete($arr_option, true);
     }
 
 
     /**
-     * 字段自增
-     * @param int $id
+     * 字段自增 - 单条数据
+     *
+     * @param int    $id
      * @param string $field
-     * @param int $step
+     * @param int    $step
+     *
      * @return mixed
      */
-    public function increaseNum( $id, $field = 'sort', $step = 1)
+    public function increase_num($id, $field = 'sort', $step = 1)
     {
-        return $this->laravel_child_model()->where('id', $id)->increment($field, $step);
+        return $this->laravel_model->where('id', $id)
+                                   ->increment($field, $step);
     }
 
     /**
-     * 字段自减
-     * @param int $id
+     * 字段自减 - 单条数据
+     *
+     * @param int    $id
      * @param string $field
-     * @param int $step
+     * @param int    $step
+     *
      * @return mixed
      */
-    public function decreaseNum( $id, $field = 'sort', $step = 1)
+    public function decrease_num($id, $field = 'sort', $step = 1)
     {
-        return $this->laravel_child_model()->where('id', $id)->decrement($field, $step);
+        return $this->laravel_model->where('id', $id)
+                                   ->decrement($field, $step);
+    }
+
+
+    /**
+     * 字段自增 - 根据条件多条数据
+     *
+     * @param        $option
+     * @param string $field
+     * @param int    $step
+     *
+     * @return mixed
+     * @author wumengmeng <wu_mengmeng@foxmail.com>
+     */
+    public function incr_num($option, $field = 'sort', $step = 1)
+    {
+        $result = $this->laravel_model->laravelWhereOption($option)
+                                      ->increment($field, $step);
+        return $result;
+    }
+
+    /**
+     * 字段自减 - 根据条件多条数据
+     *
+     * @param        $option
+     * @param string $field
+     * @param int    $step
+     *
+     * @return mixed
+     * @author wumengmeng <wu_mengmeng@foxmail.com>
+     */
+    public function decr_num($option, $field = 'sort', $step = 1)
+    {
+        $result = $this->laravel_model->laravelWhereOption($option)
+                                      ->decrement($field, $step);
+        return $result;
     }
 
 }
